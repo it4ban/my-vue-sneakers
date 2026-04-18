55a7378259252b942deb8103bf89869b17eb42f0
Alex Yagodkin <adamsHillzzz@gmail.com>
2026-04-18 19:59:47 +0300

    feat: add filter select sneakers and search input

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,33 @@ import DrawerCart from './components/DrawerCart.vue'
 </script>
 
 <template>
-  <DrawerCart />
+  <!-- <DrawerCart /> -->
 
   <div class="w-4/5 mx-auto bg-white rounded-xl shadow-xl mt-14">
     <AppHeader />
 
     <div class="p-10">
-      <h1 class="text-3xl font-bold mb-10">Все кроссовки</h1>
+      <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold mb-10">Все кроссовки</h1>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none border-slate-200">
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
+
+            <input
+              class="border border-slate-200 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск..."
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
